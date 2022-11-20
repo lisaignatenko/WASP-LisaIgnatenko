27c90120548f382fe5bbcb3dc464fa0922956fda
lisaignatenko <96386736+lisaignatenko@users.noreply.github.com>
2022-11-20 17:27:31 +0300

    Add files via upload

---
+++ hw1_1_1.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=1;i<7; i++)
+            {
+                for (int j=i+1;j<8;j++ )
+                {
+                    for (int k=j+1;k<9;k++ )
+                    {
+                        int l = k + 1;
+                        Console.WriteLine($"{i}{j}{k}{l}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
